cdb293d51e6fd5b9d4bf3375eb405d49966c4e97
khietltn <42233194+khietltn@users.noreply.github.com>
2023-10-10 21:14:17 +0700

    Buoi05-Vi du ve nhan - goto
    Vi dụ về nhập năm sinh và tính ngày

--- CGO_Buoi04/Program.cs
+++ CGO_Buoi04/Program.cs
@@ -7,21 +7,39 @@ namespace CGO_Buoi04
     {
         static void Main(string[] args)
         {
-            //a+b= ? => 2+3=5
-            int a, b;
-            a = int.Parse( Console.ReadLine());
-            b = int.Parse(Console.ReadLine());
-            //C1: Ghep chuoi
-            Console.WriteLine(a + " + " + b + " = " + (a+b));
 
-            //C2: Dinh dang chuoi
-            Console.WriteLine("{0} + {1} = {2}",  a,   b,a+b);
-            Console.WriteLine("{1} + {2} = {0}", a+b, a , b);
+
+            ViDuXuatChuoi();
+            //XuatChuoi();
 
             Console.ReadKey();  //tam dung man hinh
         }
 
-        
+        private static void ViDuXuatChuoi()
+        {
+            Console.WriteLine(@"Select one option: 
+    Press 1 if you want to exchange VND to USD
+    Press 2 if you want to exchange USD to VND");
+
+            Console.WriteLine("Select one option: " +
+                            "\n Press 1 if you want to exchange VND to USD " +
+                            "\n Press 2 if you want to exchange USD to VND");
+        }
+
+        private static void XuatChuoi()
+        {
+            //a+b= ? => 2+3=5
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            //C1: Ghep chuoi
+            Console.WriteLine(a + " + " + b + " = " + (a + b));
+
+            //C2: Dinh dang chuoi
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            Console.WriteLine("{1} + {2} = {0}", a + b, a, b);
+        }
+
     }
     
 }
---
+++ CGO_Buoi05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tuoi=-1;        //while , do _ while
+            int luot_dem = 3;   //vi du chi duoc nhap 3 lan
+        nhap_du_lieu:
+            Console.Write("Nhap tuoi: ");
+            //tuoi = int.Parse(Console.ReadLine());
+            if (int.TryParse(Console.ReadLine(), out tuoi) && tuoi >0 && tuoi <=120){
+                //Xu ly du lieu tuoi
+                Console.WriteLine("Tuoi da nhap la " + tuoi);
+                Console.WriteLine("So ngay da song la: {0}", tuoi*365); //Hoan tat yeu ve DUNG
+            }else {
+                luot_dem--;
+                if(luot_dem == 0)
+                {
+                    Console.WriteLine("Qua so lan thu lai!");
+                    
+                    goto nhiem_vu;
+                }
+                Console.WriteLine("Tuoi da nhap ko hop le. Luot nhap con lai {0}. Moi ban nhap lai.!", luot_dem);
+                goto nhap_du_lieu;
+            }
+        nhiem_vu:
+            Console.WriteLine("======================================");
+            Console.ReadKey();
+        }
+    }
+}
