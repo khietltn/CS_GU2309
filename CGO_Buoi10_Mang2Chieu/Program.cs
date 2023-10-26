80fde23fed0062405b7a3efde0613f7790bfd788
khietltn <42233194+khietltn@users.noreply.github.com>
2023-10-26 21:31:56 +0700

    Update Program.cs
    Sửa bài tập:
    Xuất mảng với điều kiện số lẻ nhân đôi giá trị, số chẵn giữ nguyên

--- CGO_Buoi10_Mang2Chieu/Program.cs
+++ CGO_Buoi10_Mang2Chieu/Program.cs
@@ -18,10 +18,10 @@ namespace CGO_Buoi10_Mang2Chieu
             //ArrayList<>
             List<int> lst = new List<int>();//lst={}
             //nap du lieu tu dong 10 so
-            int n = 10;
+            int n = 5;
             Random rd = new Random();
             for (int i = 0; i < n; i++)
-                lst.Add(rd.Next(1, 50));
+                lst.Add(i+1);
             //xuat ra man hinh truoc
             Console.WriteLine(string.Join(" ", lst));
             //Q1: Xuat ra man hinh so chan?
@@ -39,10 +39,10 @@ namespace CGO_Buoi10_Mang2Chieu
                 lst.Where(item =>isChinhPhuong(item))));
             **/
             //Nhan cac so len 2
-            Console.WriteLine(string.Join(" ",
-                lst.Select(item => item*2)));
-            //1 2 3 4 5
-            //2 2 6 4 10
+            //Console.WriteLine(string.Join(" ",
+            //    lst.Select(item => item*2)));
+            //input=  1 2 3 4 5
+            //output= 2 2 6 4 10
             //Nhan 2 cho cac so le ???
             foreach (int item in lst)
             {
@@ -50,9 +50,12 @@ namespace CGO_Buoi10_Mang2Chieu
                     Console.Write(item * 2 + " ");
                 else Console.Write(item + " ");
              }
-            Console.WriteLine(string.Join(" ",
-                lst.Select(item => item * 2)));
-
+            Console.WriteLine("\n");
+            //by ThaoNguyen
+            var items = lst.Select(m => m % 2 != 0 ? m * 2 : m);
+            Console.WriteLine(string.Join(" ", items));
+            //toan tu ba ngoi  a ? b : c
+            //<=> if (a) do (b) else do (c)
             Console.ReadKey();
         }
     }
